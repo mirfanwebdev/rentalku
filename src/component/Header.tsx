316ab6bc0c8d660cb7644ea1d58ea0b62862c4ea
m.irfan <m.irfanwebdev@gmail.com>
2025-01-08 17:22:58 +0700

    add: hamburger menu for mobile screen

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,16 +1,18 @@
-import { Flex } from "antd";
+import { Flex, Grid } from "antd";
 import Navbar from "./Navbar";
-import './Header.scss'
+import './Header.scss';
+const { useBreakpoint } = Grid
 
 interface HeaderProps {
     children?: JSX.Element
 }
 
 const Header = ({ children }: HeaderProps) => {
+    const screens = useBreakpoint();
     return (
         <>
             <header>
-                <Flex justify="space-between" align="center" wrap >
+                <Flex justify="space-between" align={screens.xs ? 'flex-start' : 'center'} wrap >
                     <Navbar />
                 </Flex>
                 {children && <div>{children}</div>}
