848d6ca9d620a30a95bde5baf7afa6db12a7a419
m.irfan <m.irfanwebdev@gmail.com>
2025-03-14 15:28:16 +0700

    fix: api error handling

--- src/pages/CarListPage/ContainerList.tsx
+++ src/pages/CarListPage/ContainerList.tsx
@@ -7,33 +7,47 @@ interface ContainerListProps {
     cars: CarItem[]
     current: number
     total?: number
+    error: boolean
+    isLoading: boolean
     handlePage: (page: number) => void
 }
 
-const ContainerList = ({ cars, current, total, handlePage }: ContainerListProps) => {
+const ContainerList = ({ cars, current, total, error, isLoading, handlePage }: ContainerListProps) => {
     const screens = useBreakpoint()
 
     return (
         <>
-            <Row gutter={[{ xs: 8, md: 24 }, 16]} style={{ paddingTop: screens.xs ? '1rem' : '7.5rem' }}>
-                {cars.map((item) => (
-                    <Col key={item.id} span={screens.xs ? 12 : 8}>
-                        <CardCar
-                            image={item.image}
-                            name={item.name}
-                            price={item.price}
-                            id={item.id}
-                        />
-                    </Col>
-                ))}
-            </Row>
-            <Pagination
-                defaultCurrent={1}
-                current={current}
-                onChange={handlePage}
-                pageSize={10}
-                total={total}
-            />
+            {isLoading ?
+                <h1 style={{ paddingTop: screens.xs ? '1rem' : '7rem' }}>Loading.....</h1>
+                :
+                <>
+                    {error ?
+                        <h1 style={{ paddingTop: screens.xs ? '1rem' : '7rem' }}>505: Internal Server Error</h1>
+                        :
+                        <>
+                            <Row gutter={[{ xs: 8, md: 24 }, 16]} style={{ paddingTop: screens.xs ? '1rem' : '7.5rem' }}>
+                                {cars.map((item) => (
+                                    <Col key={item.id} span={screens.xs ? 12 : 8}>
+                                        <CardCar
+                                            image={item.image}
+                                            name={item.name}
+                                            price={item.price}
+                                            id={item.id}
+                                        />
+                                    </Col>
+                                ))}
+                            </Row>
+                            <Pagination
+                                defaultCurrent={1}
+                                current={current}
+                                onChange={handlePage}
+                                pageSize={10}
+                                total={total}
+                            />
+                        </>
+                    }
+                </>
+            }
         </>
     )
 }
